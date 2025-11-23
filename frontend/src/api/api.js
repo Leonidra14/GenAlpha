fcfed5d53769bc8e3cd02623385f6e61bf57a6be
Leonidra14 <hana.sumsalova@gmail.com>
2025-11-23 16:58:37 +0100

    updated api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,5 +1,12 @@
 export const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8000";
 
+
+export async function getTeacherClasses(teacherId = 1) {
+  const res = await fetch(`${API_URL}/classes/teacher/${teacherId}`);
+  if (!res.ok) throw new Error("Nepodařilo se načíst třídy");
+  return await res.json();
+}
+
 export async function ping() {
   const r = await fetch(`${API_URL}/health`);
   if (!r.ok) throw new Error("API health failed");
